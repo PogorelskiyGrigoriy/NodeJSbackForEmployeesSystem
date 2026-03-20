c4b5565e60a4ac08546ef4b5a3da451eebf261be
PogorelskiyGrigoriy <pogorelskiygrigoriy@gmail.com>
2026-03-20 11:19:23 +0200

    refactor: add small fixes into index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,13 @@ import FilterStream from "./FilterStream.js";
 import LimitStream from "./LimitStream.js";
 import OutputStream from "./OutputStream.js";
 import UniqueStream from "./UniqueStream.js";
+import logger from "./pino-logger.js";
 
 async function run() {
     const source = new InfiniteRandomStream(1, 100);
     const filter = new FilterStream(n => n % 2 === 0);
     const unique = new UniqueStream();
-    const limit = new LimitStream(45); 
+    const limit = new LimitStream(20); 
     const output = new OutputStream("; ");
     
 
@@ -21,13 +22,9 @@ async function run() {
             limit,
             output
         );
-        console.log("\n\n✅ Done! Stream stopped by LimitStream.");
     } catch (err: any) {
     if (err.code === 'ERR_STREAM_PREMATURE_CLOSE') {
-        // Используем setImmediate, чтобы дождаться очистки буферов stdout
-        setImmediate(() => {
             console.log("\n✅ Done! Stream stopped by LimitStream.");
-        });
     } else {
         console.error("❌ Pipeline failed:", err);
     }
